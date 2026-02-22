2aab8300aec773ff3c55c994fb6f9efba9984faf
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2026-02-22 16:34:55 +0100

    fix: Fix copyright statement

--- internal/server/post_handler_test.go
+++ internal/server/post_handler_test.go
@@ -1,4 +1,4 @@
-// Copyright 2026 Jip de Beer (Jip-Hop) and ojster contributers
+// Copyright 2026 Jip de Beer (Jip-Hop) and Ojster contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
